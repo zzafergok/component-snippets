397b8ae7d37be49ef3f3560d9155ea2b4dfa1501
zzafergok <zfrgk96@gmail.com>
2022-03-09 23:54:51 +0300

    added `SubHeader` and `FormContainer` component

--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import SubHeader from "../block/sub_header";
+import FormContainer from "../components/form_container";
 
 function Components() {
-  return <>Components</>;
+  return (
+    <>
+      <SubHeader
+        leftText="Black Lives Matter"
+        rightText="Stop war in Ukraine"
+      />
+
+      <FormContainer />
+    </>
+  );
 }
 
 export default Components;
