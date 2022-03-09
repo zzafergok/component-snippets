6cedb4d2519abb80c1fa1d803444064f5016f88c
zzafergok <zfrgk96@gmail.com>
2022-03-09 23:54:16 +0300

    added `SubHeader` component

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,15 @@
 import Header from "../block/header";
+import SubHeader from "../block/sub_header";
 
 function Contact() {
-  return <>Contact</>;
+  return (
+    <>
+      <SubHeader
+        leftText="Black Lives Matter"
+        rightText="Stop war in Ukraine"
+      />
+    </>
+  );
 }
 
 export default Contact;
--- src/pages/documents.tsx
+++ src/pages/documents.tsx
@@ -1,7 +1,15 @@
 import Header from "../block/header";
+import SubHeader from "../block/sub_header";
 
 function Documents() {
-  return <>Document</>;
+  return (
+    <>
+      <SubHeader
+        leftText="Black Lives Matter"
+        rightText="Stop war in Ukraine"
+      />
+    </>
+  );
 }
 
 export default Documents;
