45b429fef704d2e5d052833862b2105ea7caee9d
zzafergok <zfrgk96@gmail.com>
2022-03-10 12:16:39 +0300

    added global remiz icon css

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "../src/assets/styles/index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../src/assets/styles/index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+import 'remixicon/fonts/remixicon.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+	document.getElementById('root')
 );
