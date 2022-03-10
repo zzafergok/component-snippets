d48672acfa277b65443ffdfe3f57e28fb33d0c13
zzafergok <zfrgk96@gmail.com>
2022-03-10 12:03:51 +0300

    prettier format

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,19 @@
-import Header from "../block/header";
-import InputIndex from "../components/input/index";
-import ButtonIndex from "../components/button/index";
-import SubHeader from "../block/sub_header";
+import Header from '../block/header';
+import InputIndex from '../components/input/index';
+import ButtonIndex from '../components/button/index';
+import SubHeader from '../block/sub_header';
 
 function Home() {
-  return (
-    <>
-      <SubHeader
-        leftText="Black Lives Matter"
-        rightText="Stop war in Ukraine"
-      />
-      <ButtonIndex />
-      <InputIndex />
-    </>
-  );
+	return (
+		<>
+			<SubHeader
+				leftText="Black Lives Matter"
+				rightText="Stop war in Ukraine"
+			/>
+			<ButtonIndex />
+			<InputIndex />
+		</>
+	);
 }
 
 export default Home;
