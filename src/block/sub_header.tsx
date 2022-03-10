779dc7775049ce35888d857cb4a1d146f2b96977
zzafergok <zfrgk96@gmail.com>
2022-03-10 12:03:38 +0300

    prettier format and changed bg color

--- src/block/sub_header.tsx
+++ src/block/sub_header.tsx
@@ -2,50 +2,50 @@
 import { css } from "@emotion/react";
 
 type Props = {
-  leftText?: string;
-  rightText?: string;
+	leftText?: string;
+	rightText?: string;
 };
 
 function SubHeader(Props) {
-  const { leftText, rightText } = Props;
+	const { leftText, rightText } = Props;
 
-  const containerStyle = css`
-    div {
-      display: grid;
-      grid-template-columns: 3fr 3fr;
+	const containerStyle = css`
+		div {
+			display: grid;
+			grid-template-columns: 3fr 3fr;
 
-      div {
-        text-decoration: underline;
-        color: var(--input-border-color);
-        font-weight: 500;
-      }
-    }
-  `;
+			div {
+				text-decoration: underline;
+				color: var(--input-border-color);
+				font-weight: 500;
+			}
+		}
+	`;
 
-  const subHeaderStyle = css`
-    width: 100%;
-    background-color: #f2f2f2;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: 2rem;
-    height: 2.5rem;
-  `;
+	const subHeaderStyle = css`
+		width: 100%;
+		background-color: #04293a;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin-bottom: 2rem;
+		height: 2.5rem;
+	`;
 
-  const rightTextStyle = css`
-    display: flex !important;
-    justify-content: flex-end;
-  `;
-  return (
-    <div css={subHeaderStyle}>
-      <div css={containerStyle}>
-        <div>
-          <div>{leftText}</div>
-          <div css={rightTextStyle}>{rightText}</div>
-        </div>
-      </div>
-    </div>
-  );
+	const rightTextStyle = css`
+		display: flex !important;
+		justify-content: flex-end;
+	`;
+	return (
+		<div css={subHeaderStyle}>
+			<div css={containerStyle}>
+				<div>
+					<div>{leftText}</div>
+					<div css={rightTextStyle}>{rightText}</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default SubHeader;
