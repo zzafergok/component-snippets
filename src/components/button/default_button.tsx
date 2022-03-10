6f75cb77f14572efffec6e91bbe4e435238c7a4c
zzafergok <zfrgk96@gmail.com>
2022-03-10 18:00:23 +0300

    prettier format and fix conditions

--- src/components/button/default_button.tsx
+++ src/components/button/default_button.tsx
@@ -1,35 +1,71 @@
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-import { ButtonProps } from "../../modules/button.ts";
+import { css } from '@emotion/react';
+import { ButtonProps } from '../../modules/button.ts';
 
-function DefaultButton(ButtonProps) {
-  const {
-    text,
-    onClick,
-    color,
-    backgroundColor,
-    border,
-    height,
-    width,
-    icon,
-    radius,
-  } = ButtonProps;
-  return (
-    <button
-      onClick={onClick}
-      css={{
-        color: color ? color : "white",
-        backgroundColor: backgroundColor ? backgroundColor : "black",
-        height: height ? `${height}rem` : "2.5rem",
-        width: width ? `${width}rem` : "5rem",
-        border: border ? border : "none",
-        borderRadius: radius ? `${radius}rem` : "0.5rem",
-      }}
-    >
-      {icon ? <i className={icon} /> : null}
-      {text ? text : "Button"}
-    </button>
-  );
+function DefaultButton({
+	text = 'Ok',
+	onClick,
+	color = 'white',
+	backgroundColor = 'black',
+	border = 'none',
+	height = '2.5rem',
+	width,
+	icon,
+	radius = '12px',
+}: ButtonProps) {
+	const buttonWidth = () => {
+		if (text.length < 3) {
+			return text.length * 2.25 + 'rem';
+		} else if (text.length > 3) {
+			return text.length * 1 + 'rem';
+		} else {
+			return '5rem';
+		}
+	};
+
+	console.log(buttonWidth());
+	width = buttonWidth();
+
+	const defaultButtonStyle = css`
+		color: ${color};
+		background-color: ${backgroundColor};
+		height: ${height};
+		width: ${width};
+		border: ${border};
+		border-radius: ${radius};
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	`;
+
+	const iconStyle = css`
+		margin-right: 1rem;
+	`;
+
+	const renderDefaultButton = () => {
+		if (text && !icon) {
+			return text;
+		} else if (!text && icon) {
+			return <i className={icon} css={iconStyle} />;
+		} else if (text && icon) {
+			return (
+				<>
+					<i className={icon} css={iconStyle} />
+					{text}
+				</>
+			);
+		} else {
+			return null;
+		}
+	};
+
+	return (
+		<div>
+			<button onClick={onClick} css={defaultButtonStyle}>
+				{renderDefaultButton()}
+			</button>
+		</div>
+	);
 }
 
 export default DefaultButton;
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,11 +1,16 @@
-import DefaultButton from "./default_button";
+import DefaultButton from './default_button';
 
 function ButtonIndex() {
-  return (
-    <div>
-      <DefaultButton onClick={() => alert("I am work!!!")} />
-    </div>
-  );
+	return (
+		<div>
+			<DefaultButton
+				onClick={() => alert('I am work!!!')}
+				icon="ri-knife-blood-line"
+				text="I am work"
+				width="9rem"
+			/>
+		</div>
+	);
 }
 
 export default ButtonIndex;
