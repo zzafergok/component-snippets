f2dc967bb790d59f3973782334d7d7fb2e060863
zzafergok <zfrgk96@gmail.com>
2022-03-10 17:59:56 +0300

    prettier format and added label inside to component

--- src/components/input/default_input.tsx
+++ src/components/input/default_input.tsx
@@ -1,97 +1,106 @@
 /** @jsxImportSource @emotion/react */
-import { css, CSSObject } from "@emotion/react";
-import { InputProps } from "../../modules/input";
-
-const defaultInputStyle = css`
-  border: 1px solid var(--default-border-color);
-  border-radius: 0.25rem;
-  padding: 0.5rem;
-  background-color: #fff;
-  transition: all 0.3s ease-in-out;
-
-  &:focus: {
-    border-color: var(--input-border-color);
-    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.2);
-  }
-`;
+import { css, CSSObject } from '@emotion/react';
+import { InputProps } from '../../modules/input';
 
 function DefaultInput(InputProps) {
-  const {
-    className,
-    type = "text" as any,
-    id,
-    name,
-    value,
-    placeholder,
-    onChange,
-    onBlur,
-    onFocus,
-    onKeyPress,
-    disabled = false,
-    required = false,
-    min,
-    max,
-    step,
-    minLength,
-    maxLength,
-    pattern,
-    size,
-    autoComplete = "off",
-    autoFocus = false,
-    spellCheck,
-    autoSave,
-    list,
-    multiple,
-    alt,
-    src,
-    width = "40",
-    height = "2",
-    icon,
-    defaultValue,
-  } = InputProps;
+	const {
+		className,
+		type = 'text' as any,
+		id,
+		name,
+		value,
+		placeholder,
+		onChange,
+		onBlur,
+		onFocus,
+		onKeyPress,
+		disabled = false,
+		required = false,
+		min,
+		max,
+		step,
+		minLength,
+		maxLength,
+		pattern,
+		size,
+		autoComplete = 'off',
+		autoFocus = false,
+		spellCheck,
+		autoSave,
+		list,
+		multiple,
+		alt,
+		src,
+		width = '20%',
+		height = '1.5rem',
+		icon,
+		defaultValue,
+		label,
+	} = InputProps;
 
-  return (
-    <div>
-      <input
-        css={
-          (defaultInputStyle,
-          {
-            width: width && `${width}%`,
-            height: height && `${height}rem`,
-          })
-        }
-        className={className}
-        type={type}
-        id={id}
-        name={name}
-        value={value}
-        defaultValue={defaultValue}
-        placeholder={placeholder}
-        onBlur={onBlur}
-        onFocus={onFocus}
-        onKeyPress={onKeyPress}
-        onChange={onChange}
-        disabled={disabled}
-        required={required}
-        min={min}
-        max={max}
-        step={step}
-        minLength={minLength}
-        maxLength={maxLength}
-        pattern={pattern}
-        size={size}
-        autoComplete={autoComplete}
-        autoFocus={autoFocus}
-        spellCheck={spellCheck}
-        autoSave={autoSave}
-        list={list}
-        multiple={multiple}
-        alt={alt}
-        src={src}
-        icon={icon}
-      />
-    </div>
-  );
+	const defaultInputStyle = css`
+		border: 1px solid var(--default-border-color);
+		border-radius: 0.25rem;
+		padding: 0.5rem;
+		background-color: #fff;
+		transition: all 0.3s ease-in-out;
+		width: ${width};
+		height: ${height};
+		margin-bottom: 1rem;
+
+		&:focus: {
+			border-color: var(--input-border-color);
+			box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.2);
+		}
+	`;
+
+	const labelStyle = css`
+		display: block;
+		margin-bottom: 0.25rem;
+		font-size: 11pt;
+	`;
+
+	return (
+		<div>
+			{id && label && (
+				<label htmlFor={id} css={labelStyle}>
+					{label}
+				</label>
+			)}
+			<input
+				css={defaultInputStyle}
+				className={className}
+				type={type}
+				id={id}
+				name={name}
+				value={value}
+				defaultValue={defaultValue}
+				placeholder={placeholder}
+				onBlur={onBlur}
+				onFocus={onFocus}
+				onKeyPress={onKeyPress}
+				onChange={onChange}
+				disabled={disabled}
+				required={required}
+				min={min}
+				max={max}
+				step={step}
+				minLength={minLength}
+				maxLength={maxLength}
+				pattern={pattern}
+				size={size}
+				autoComplete={autoComplete}
+				autoFocus={autoFocus}
+				spellCheck={spellCheck}
+				autoSave={autoSave}
+				list={list}
+				multiple={multiple}
+				alt={alt}
+				src={src}
+				icon={icon}
+			/>
+		</div>
+	);
 }
 
 export default DefaultInput;
--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,20 +1,18 @@
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-import DefaultInput from "./default_input";
+import DefaultInput from './default_input';
 
 function InputIndex() {
-  const spaceInput = css`
-    input {
-      margin-bottom: 1rem;
-    }
-  `;
-  return (
-    <div css={spaceInput}>
-      <DefaultInput type="password" placeholder="Your Password" />
-      <DefaultInput type="text" defaultValue="Your Name" />
-      <DefaultInput type="text" width="25" height="1.5" />
-    </div>
-  );
+	return (
+		<div>
+			<DefaultInput type="password" placeholder="Your Password" />
+			<DefaultInput
+				type="text"
+				id="try"
+				label="Default Input"
+				defaultValue="Your Name"
+			/>
+			<DefaultInput type="text" width="25" height="1.5" />
+		</div>
+	);
 }
 
 export default InputIndex;
