4ee5098430b54792d8528c69d9ec0f8ccf05b763
zzafergok <zfrgk96@gmail.com>
2022-03-15 12:09:52 +0300

    added new routes

--- src/all_routes.tsx
+++ src/all_routes.tsx
@@ -1,26 +1,38 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import Home from "./pages/home";
-import Components from "./pages/components";
-import Documents from "./pages/documents";
-import Contact from "./pages/contact";
-import Index from "./layout/index";
+import Home from './pages/home';
+import Components from './pages/components';
+import Documents from './pages/documents';
+import Contact from './pages/contact';
+
+import InputIndex from './components/input/index';
+import RadioIndex from './components/radio/index';
+import SwitchIndex from './components/switch/index';
+import ButtonIndex from './components/button/index';
+import FormIndex from './components/form/index';
+
+import Index from './layout/index';
 
 type Props = {};
 
 function AllRoutes({}: Props) {
-  return (
-    <>
-      <Index>
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="components" element={<Components />} />
-          <Route path="documents" element={<Documents />} />
-          <Route path="contact" element={<Contact />} />
-        </Routes>
-      </Index>
-    </>
-  );
+	return (
+		<>
+			<Index>
+				<Routes>
+					<Route path="/" exact element={<Home />} />
+					<Route path="components" exact element={<Components />} />
+					<Route path="documents" element={<Documents />} />
+					<Route path="contact" element={<Contact />} />
+					<Route path="components/input" element={<InputIndex />} />
+					<Route path="components/radio" element={<RadioIndex />} />
+					<Route path="components/switch" element={<SwitchIndex />} />
+					<Route path="components/button" element={<ButtonIndex />} />
+					<Route path="components/form" element={<FormIndex />} />
+				</Routes>
+			</Index>
+		</>
+	);
 }
 
 export default AllRoutes;
